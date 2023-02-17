c0fb38ef1cf938db8d2ae5606c742f303b2bd3f0
Nweke <annypaul80@gmail.com>
2023-02-17 12:42:59 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Program that print a line to the standard error
+ *
+ * Return: 1 is success
+ */
+
+int main(void)
+
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return(1);
+}
