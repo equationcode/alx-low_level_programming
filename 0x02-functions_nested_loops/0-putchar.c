9c3cb8dd2d615f937df34240c9b16c7b69f931c0
Nweke <annypaul80@gmail.com>
2023-02-21 07:04:11 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * _putchar: print an alphabets putchar
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
