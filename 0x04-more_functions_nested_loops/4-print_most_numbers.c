117517bbe5781618cf4fa681ba10747ba08730e3
Nweke <annypaul80@gmail.com>
2023-02-23 16:45:34 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,14 +11,14 @@ void print_most_number(void)
 	int x = 0;
 	for (; x <= 9; x++)
 	{
-		if (x == 2 || x == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(x + '0');
-		}
+	if (x == 2 || x == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(x + '0');
+	}
 	}
 	_putchar('\n);
 }
