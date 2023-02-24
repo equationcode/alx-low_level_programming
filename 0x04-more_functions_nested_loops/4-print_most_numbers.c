42f3fc25200f375398b9c7ee4184b42cc9622487
Nweke <annypaul80@gmail.com>
2023-02-23 16:54:12 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * print_most_numbers - function that prints numbers from 0 to 9
- * Description: prints numbers excluding 2 and 4
+ * Description: print numbers excluding 2 and 4
  * Return: numbers from 0 to 9
  */
 
-void print_most_number(void)
+void print_most_numbers(void)
 {
 	int x = 0;
+
 	for (; x <= 9; x++)
 	{
 	if (x == 2 || x == 4)
