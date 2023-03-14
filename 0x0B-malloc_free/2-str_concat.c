847e66c9ff97f90355330ce1b98f2580f8ca4ade
Nweke <annypaul80@gmail.com>
2023-03-14 07:04:33 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,6 +30,9 @@ char *str_concat(char *s1, char *s2)
 	l = i + j;
 	s = malloc((sizeof(char) * l) + 1);
 
+	if (s == NULL)
+		return (NULL);
+
 	j = 0;
 
 	while (k < l)
@@ -42,6 +45,7 @@ char *str_concat(char *s1, char *s2)
 			s[k] = s2[j];
 			j++;
 		}
+		k++;
 	}
 
 	s[k] = '\0';
