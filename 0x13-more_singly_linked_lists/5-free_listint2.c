d1a4d82fe54d312d8cecf237b46fe03a6cd83fa3
equationcode <annypaul80@gmail.com>
2023-04-04 15:42:00 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a linked list
+ * @head: the first listint_t list to be freed
+ *
+ * Return: 0 on success
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		free(*head);
+		temp = (*head)->next;
+		*head = temp;
+	}
+	*head = NULL;
+}
