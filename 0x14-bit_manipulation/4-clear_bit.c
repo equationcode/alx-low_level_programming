d611af48c5c25de8a94d558110a7669ad4923f7d
equationcode <annypaul80@gmail.com>
2023-04-11 18:02:54 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the value of a bit to 0
+ * at a given index
+ * @n: the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
