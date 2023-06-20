4e01f2ee109dcfb46658062a8c6d0683ec7ab818
equationcode <annypaul80@gmail.com>
2023-06-20 02:59:54 -0700

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O /tmp/cracking_output.so https://raw.github.com/equationcode/alx-low_level_programming/main/0x18-dynamic_libraries/cracking_output.so
+export LD_PRELOAD=/tmp/cracking_output.so
